96a475a6a22660fe47e55bffcdaf180187d96c2f
liuxianglin <943572695@qq.com>
2018-03-27 23:10:57 +0800

    add the axios

---
+++ src/utils/request.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+var instance = axios.create({
+    baseURL: 'https://some-domain.com/api/',
+    timeout: 1000,
+    headers: {'X-Custom-Header': 'foobar'}
+});
+
+export default instance
